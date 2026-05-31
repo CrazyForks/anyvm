c99567327d70d0b9142b1b78f26b76aedc594cba
neil <gitpc@neilpang.com>
2026-05-31 17:15:47 +0200

    update for openbsd [testos=openbsd]

--- anyvm.py
+++ anyvm.py
@@ -112,7 +112,7 @@ OPENBSD_E1000_RELEASES = {"7.3", "7.4", "7.5", "7.6"}
 
 DEFAULT_BUILDER_VERSIONS = {
     "freebsd": "2.1.3",
-    "openbsd": "2.0.2",
+    "openbsd": "2.0.3",
     "netbsd": "2.0.3",
     "dragonflybsd": "2.0.4",
     "solaris": "2.0.5",
